e40006b9020d386f4b5d059f0146e6f527372eb3
Fresh Prince <mario.n.slavov2004@gmail.com>
2024-11-02 12:51:40 +0200

    Update Connection String

---
+++ GamersShopingListC#MVC/Migrations/20241102104940_new divice.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamersShopingListC_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class newdivice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
